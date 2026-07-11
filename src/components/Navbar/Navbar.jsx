8f5900abb223cda55ac72212c868bf2e610a83aa
shohan-28 <shohanshawon551@gmail.com>
2026-07-11 23:49:02 +0600

    Updated some files and fixed bugs

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { LuShoppingCart } from '../../../node_modules/react-icons/lu';
+
+
 
 const Navbar = () => {
     return (
         <div className="bg-transparent w-[90%] mx-auto">
-            <div>
-                <div>
-                    <img src="/src/assets" alt="" />
+            <div className="flex justify-between p-4 rounded-l-3xl rounded-r-3xl">
+                <div className="">
+                    <img src="/src/assets/Unofficial_JavaScript_logo_2.svg.png" alt="" className="h-13 w-full object-contain" />
                 </div>
-                <div></div>
+                <div className="bg-amber-500 rounded-t-2xl">
+                    <p className="bg-amber-200 rounded-t-2xl text-3xl p-2"><LuShoppingCart /></p>
+                </div>
             </div>
             <div className="flex justify-center lg:gap-15 md:gap-10 gap-5 bg-gray-200/30 p-4 rounded-l-3xl rounded-r-3xl font-bold text-black text-base lg:text-lg md:text-lg">
                 <a href="#">Home</a>
