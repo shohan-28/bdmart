270a7a0a24f6c8b60200c858bf858703c64cbc89
shohan-28 <shohanshawon551@gmail.com>
2026-08-05 13:41:06 +0600

    Updated some files and fixed bugs

--- src/components/CheckOut/CheckOut.jsx
+++ src/components/CheckOut/CheckOut.jsx
@@ -67,7 +67,7 @@ const Checkout = () => {
   }
 
   try {
-    const res = await fetch("http://localhost:5000/api/orders", {
+    const res = await fetch("https://sprienge-backend.onrender.com/api/orders", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
